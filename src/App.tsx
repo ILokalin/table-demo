7a83aa7e82f12545a858537f8f7891251c2ac15a
lokalin <ilya-1974@yandex.ru>
2023-03-19 22:24:27 +0600

    feat: add routes and 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import PrivateOutlet from './components/misc/PrivateOutlet';
+import PageNotFound from './containers/PageNotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Routes>
+      <Route element={<PrivateOutlet />}>
+        <Route path="/sample1" element={<h1>Hello</h1>} />
+      </Route>
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </div>
+);
 
 export default App;
---
+++ src/containers/PageNotFound/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../../components/Buttons/Button';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f8f8f8;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 4rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+  color: #444;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 2rem;
+  font-weight: normal;
+  color: #666;
+`;
+
+const NotFoundPage = () => (
+  <Container>
+    <Wrapper>
+      <Title>404</Title>
+      <Subtitle>Page not found</Subtitle>
+      <Button>Go Back Home</Button>
+    </Wrapper>
+  </Container>
+);
+
+export default NotFoundPage;
